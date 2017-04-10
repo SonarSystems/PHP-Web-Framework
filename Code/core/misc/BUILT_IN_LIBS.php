4877b231b27b7cfc4ee0ac9e6307b642b68b6fdb
Sonar Systems <SonarSystems@users.noreply.github.com>
2017-04-10 13:50:46 +0100

    Update BUILT_IN_LIBS.php

--- Code/core/misc/BUILT_IN_LIBS.php
+++ Code/core/misc/BUILT_IN_LIBS.php
@@ -6,13 +6,15 @@ $__CSS_BIL_LIBS = array
 (
     array( 'Bootstrap-V3-3-7', '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />' ),
 
+    array( 'Bootstrap-V4.0.0-alpha.6', '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous" />' ),
+
     array( 'Skeleton-V2-0-4', '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" />' ),
 
     array( 'Foundation-V6-2-3', '<link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.3/foundation.min.css" />' ),
 
     array( 'W3.CSS', '<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css" />' ),
 
-    array( 'Materialize-V0-97-7', '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">' ),
+    array( 'Materialize-V0-97-7', '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css" />' ),
 );
 
 
@@ -20,6 +22,10 @@ $__JS_BIL_LIBS = array
 (
     array( 'Bootstrap-V3-3-7', '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>' ),
     array( 'Bootstrap-V3-3-7', '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>' ),
+  
+    array( 'Bootstrap-V4.0.0-alpha.6', '<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>' ),
+    array( 'Bootstrap-V4.0.0-alpha.6', '<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>' ),
+    array( 'Bootstrap-V4.0.0-alpha.6', '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>' ),
 
     array( 'jQuery-V3-1-0', '<script   src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>' ),
     array( 'jQuery-V2-2-4', '<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>' ),
@@ -30,3 +36,6 @@ $__JS_BIL_LIBS = array
 
     array( 'Materialize-V0-97-7', '<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>' ),
 );
+
+
+
